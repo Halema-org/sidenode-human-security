f479afc0d2ada98d63cedf6e3d2d65355043cb3c
Halema-org <125642379+Halema-org@users.noreply.github.com>
2026-01-14 14:06:31 -0800

    Create types.ts

---
+++ src/types.ts
@@ -0,0 +1,22 @@
+export interface BreachData {
+  Name: string;
+  Title: string;
+  Domain: string;
+  BreachDate: string;
+  AddedDate: string;
+  ModifiedDate: string;
+  PwnCount: number;
+  Description: string;
+  LogoPath: string;
+  DataClasses: string[];
+  IsVerified: boolean;
+  IsFabricated: boolean;
+  IsSensitive: boolean;
+  IsRetired: boolean;
+  IsSpamList: boolean;
+}
+
+export interface ApiResponse {
+  Error?: string;
+  BreachDetails?: BreachData[];
+}
